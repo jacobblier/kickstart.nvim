a54e11bf619c8c23cb217fdb82747a1b8ff1ef72
jacobblier <jblier@lyft.com>
2025-03-15 19:46:38 -0400

    feat: Add Copilot and Copilot chat

--- init.lua
+++ init.lua
@@ -219,6 +219,20 @@ require('lazy').setup({
     },
   },
 
+  { 'github/copilot.vim' },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'github/copilot.vim' },
+      { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
+    },
+    build = 'make tiktoken', -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
