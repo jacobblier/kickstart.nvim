551262a3c4f48202f283237ab2ad288549b7e787
Jacob Blier <blier.jacob@hotmail.com>
2025-08-20 22:19:34 -0400

    chore: Do not autoformat protobuf files

--- init.lua
+++ init.lua
@@ -780,7 +780,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { javascript = true }
+        local disable_filetypes = { javascript = true, proto = true }
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
