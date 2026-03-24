ad1e8a829292f6cf798cfccaf213fc248fee3856
Jacob Blier <blier.jacob@hotmail.com>
2026-03-24 14:44:06 -0400

    fix: Make ts LSP config work on Arch Linux
    The error given by neovim before the change was
    ```
    The `require('lspconfig')` "framework" is deprecated, use vim.lsp.config (see :help lspconfig-nvim-0.11) instead.
    Feature will be removed in nvim-lspconfig v3.0.0
    stack traceback:
            .../.local/share/nvim/lazy/nvim-lspconfig/lua/lspconfig.lua:81: in function '__index'
            /home/jacob/.config/nvim/lua/config/lsp.lua:6: in main chunk
            [C]: in function 'require'
            /home/jacob/git/kickstart.nvim/init.lua:1203: in main chunk
    ```
    
    This commit aims to use the new API which is supported on both MacOS and
    Arch Linux.

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -2,11 +2,12 @@
 -- These configurations override or supplement the automatic Mason setup
 
 -- TypeScript/JavaScript Language Server (ts_ls)
--- Custom root_dir to work with projects that have package.json in subdirectories
-require('lspconfig').ts_ls.setup {
+-- Custom LSP Configurations
+vim.lsp.config('ts_ls', {
   capabilities = require('blink.cmp').get_lsp_capabilities(),
   root_dir = function(fname)
-    local util = require('lspconfig').util
-    return util.root_pattern('package.json', 'tsconfig.json', 'jsconfig.json')(fname) or util.root_pattern '.git'(fname) or vim.fs.dirname(fname)
+    return vim.fs.root(fname, { 'package.json', 'tsconfig.json', 'jsconfig.json', '.git' }) or vim.fs.dirname(fname)
   end,
-}
+})
+
+vim.lsp.enable 'ts_ls'
